3f180dfe6dc787398f2ea63eae2964185c3e6f4f
jakekasan <jake.kasan@gmail.com>
2018-06-12 23:16:14 +0200

    added a few supply chain classes for the prototype

--- services/ordering.py
+++ services/ordering.py
@@ -7,3 +7,29 @@ This is where orders are first generated. Here a payment action will be prompted
 class Ordering:
     def __init__(self):
         pass
+
+    def generateNewOrder(self):
+        # return a new order
+
+class Order:
+    def __init__(self,products):
+        self.products = []
+
+
+class Product:
+    def __init__(self,name,price,requirements):
+        self.name = name
+        self.price = price
+        self.requirements = requirements
+        self.fulfilled = []
+        self.completed = False
+
+    def addAction(self,action):
+        if action in self.requirements:
+            self.fulfilled.append(action)
+        else:
+            print("Error: action not in product blueprint")
+        return
+
+    def update(self):
+        pass
