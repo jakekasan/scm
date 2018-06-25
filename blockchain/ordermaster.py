b16fbfd6eba7413441bfbb56b7a5c6a2961d194f
jakekasan <jake.kasan@gmail.com>
2018-06-25 23:51:36 +0200

    began work on order master

---
+++ blockchain/ordermaster.py
@@ -0,0 +1,10 @@
+class OrderMaster:
+    def __init__(self,blockchain,actorType):
+        self.blockchain = blockchain
+        self.actorType = actorType
+
+    def getRelevantOrders(self):
+        """
+        return a list of orders relevant to me
+        """
+        pass
