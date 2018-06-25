23ad11a370a4e4370deb4d941d5d8010fc73f517
jakekasan <jake.kasan@gmail.com>
2018-06-25 23:50:51 +0200

    placeholder functions

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -1,6 +1,30 @@
 class BlockChain:
-    def __init__(self,genesisBlock):
+    def __init__(self,genesisBlock=None,blockchain=None):
         self.blocks = []
-        self.authorisors = 
+        
     
     def validateBlocks(self):
+        """
+        validate all blocks in the chain
+        """
+        pass
+
+    def buildActors(self):
+        """
+        get a list of authorised actors from blocks
+        """
+        pass
+
+    def buildOrders(self):
+        """
+        get list of orders (both outstanding and fullfilled)
+        """
+        pass
+
+    def buildMasters(self):
+        """
+        set masters from genesis block
+        """
+        pass
+
+    
